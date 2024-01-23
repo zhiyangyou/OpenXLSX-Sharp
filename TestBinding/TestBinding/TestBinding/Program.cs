e6b304aa0b0052570eb726bd0cd9de10157e8ffb
youzhiyang <1454900087@qq.com>
2024-01-23 23:41:44 +0800

    NEW: 测试用的.netcore工程

---
+++ TestBinding/TestBinding/TestBinding/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OpenXLSX.OpenXLSX;
+
+class Program
+{
+    static void Main()
+    {
+        XLDocument doc = new XLDocument() ;
+        doc.Create("./Demo02.xlsx");
+        var wks = doc.Workbook.Worksheet("Sheet1");
+
+        // Similar cell values, which are represented by XLCellValue objects, formulas are represented
+        // by XLFormula objects. They can be accessed through the XLCell interface using the .formula()
+        // member function. It should be noted, however, that the functionality of XLFormula is somewhat
+        // limited. Excel often uses 'shared' formulas, where the same formula is applied to several
+        // cells. XLFormula cannot handle shared formulas. Also, it cannot handle array formulas. This,
+        // in effect, means that XLFormula is not very useful for reading formulas from existing spread-
+        // sheets, but should rather be used to add or overwrite formulas to spreadsheets.
+
+        wks.Cell("A1").Value
+        // wks.cell("B1").value() = 1;
+        // wks.cell("C1").value() = 2;
+        // wks.cell("D1").value() = 3;
+        
+        doc.Save();
+        doc.Close();
+    }
+}
